adde62fc1caec207cd52ae7c8d1dcd978c45d7d0
Sumi <qkobr94@gmail.com>
2022-11-13 18:13:51 +0900

    create useTypedSelector to have an access to store

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 
 const RepositoriesList = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { data, loading, error } = useTypedSelector(
+    (state) => state.repositories
+  );
+  console.log(data);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e?.preventDefault();
-    searchRepositories(term); 
+    searchRepositories(term);
   };
 
   return (
@@ -16,6 +21,9 @@ const RepositoriesList = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
